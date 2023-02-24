e672362f5036925ae3e4b17c1275d0f19ac9b752
Seb <sebsmith3@gmail.com>
2023-02-24 17:02:10 +0000

    tried event threads

--- main.cpp
+++ main.cpp
@@ -1,48 +1,61 @@
 #include "mbed.h"
+#include "canmessage-mbed.h"
 #include "vcupinmapp.h"
 #include <cstdint>
-#include "can.h"
+#include "mbed_events.h"
+using namespace std::chrono_literals;
 
-//DigitalOut led(PA_5);
+#define LSB 0
+#define MSB 1
+#define UNSIGNED 0
+#define SIGNED 1
+
+CAN_message_t outMsg;
+CANMessage inMsg;
+
+Thread ms200;
+
+
+
 DigitalOut led(LED1);
-DigitalIn canFault(can1nfault);
-DigitalOut canNSTB(can1nstb,1); 
-DigitalOut canEN(can1en,1);
-AnalogOut throttleOut1(dacout1);
-AnalogOut throttleOut2(dacout2);
-AnalogIn throttleIn1(analogin3);
-AnalogIn throttleIn2(analogin4);
-AnalogIn vacSensor(analogin1);
-AnalogIn modeSwitch(analogin2);
-AnalogIn throttle1fdbk(throttlefeedback1);
-DigitalIn throttle2fdbk(throttlefeedback2);         //Needs changing back to analog in after hardware revision
-DigitalOut hbridge1out(hbridge1);
-DigitalOut hbridge2out(hbridge2);
-DigitalIn ctrlpilotin(ctrlpilot);
-DigitalIn proxpilotin(proxpilot);
-DigitalOut pwmoutput(pwmout);
-DigitalOut output1out(output1);
-DigitalOut output2out(output2);
-DigitalOut output3out(output3);
-DigitalOut output4out(output4);
-DigitalIn output12statin(output12stat);
-DigitalIn output34statin(output34stat);
-DigitalIn ignwakein(ignwake);
-DigitalOut linenableout(linenable);
-DigitalIn throt5vfdbkin(throt5vfdbk);
-DigitalIn degitalin1in(degitalin1);
-DigitalIn degitalin2in(degitalin2);
-DigitalIn degitalin3in(degitalin3);
-DigitalIn degitalin4in(degitalin4);
-DigitalOut chargenableout(chargenable);
-DigitalOut watchdogout(watchdog);
+DigitalIn canFault(M_CAN1_NFAULT);
+DigitalOut canNSTB(M_CAN1_NSTB,1); 
+DigitalOut canEN(M_CAN1_EN,1);
+AnalogOut throttleOut1(M_DAC_OUT_1);
+AnalogOut throttleOut2(M_DAC_OUT_2);
+AnalogIn throttleIn1(M_ANA_IN_3);
+AnalogIn throttleIn2(M_ANA_IN_4);
+AnalogIn analogin1(M_ANA_IN_1);
+AnalogIn analogin2(M_ANA_IN_2);
+AnalogIn throttle1fdbk(M_DAC_FDBK_1);
+DigitalIn throttle2fdbk(M_DAC_FDBK_2);         //Needs changing back to analog in after hardware revision
+DigitalOut hbridge1(H_BRIDGE_IN_1);
+DigitalOut hbridge2(H_BRIDGE_IN_2);
+DigitalIn ctrlpilot(M_CTRL_PILOT);
+DigitalIn proxpilot(M_PROX_PILOT);
+DigitalOut pwmout(M_PWM_OUT);
+DigitalOut output1(OUT1_EN);
+DigitalOut output2(OUT2_EN);
+DigitalOut output3(OUT3_EN);
+DigitalOut output4(OUT4_EN);
+DigitalIn output12stat(OUT1_2_STAT);
+DigitalIn output34stat(OUT3_4_STAT);
+DigitalIn ignwake(M_IGN_WAKE);
+DigitalOut linenable(M_LIN_EN);
+DigitalIn throt5vfdbkin(M_THROT_5V);
+DigitalIn degitalin1(M_DIG_IN_1);
+DigitalIn degitalin2(M_DIG_IN_2);
+DigitalIn degitalin3(M_DIG_IN_3);
+DigitalIn degitalin4(M_DIG_IN_4);
+DigitalOut chargenable(CHRG_EN);
+DigitalOut watchdog(WDI);
 
 uint16_t analoginthrot1mv, analoginthrot2mv = 0;
+uint16_t dacout1mv = 2500;
+uint16_t dacout2mv = 5000;
 
-
-CAN can1(can1rx, can1tx, 500000);       //Public CAN to Vehicle
-CAN can2(can2rx, can2tx, 500000);       //Private CAN to Tesla LDU
-
+CAN can1(M_CAN_RXD, M_CAN_TXD, 500000);       //Public CAN to Vehicle
+CAN can2(M_CAN2_RXD, M_CAN2_TXD, 500000);     //Private CAN to Tesla LDU
 
 #define repeatTime     100ms
 
@@ -52,11 +65,13 @@ void flashled(){
 
 void testDAC(){
     if (throt5vfdbkin) {
-        throttleOut1.write(0.757f);     //full range
-        throttleOut2.write(0.3785f);    //half range
+        throttleOut1.write_u16(dacout1mv * 6.5534);         //65535 (full 16 bit) devided by 2 due to dac ration of double
+        throttleOut2.write_u16(dacout2mv * 6.5534);         //(65535/2)/5000 = 6.5534
+        //throttleOut1.write(0.757f);     //full range (5v out)
+        //throttleOut2.write(0.3785f);    //half range (2.5v out)
     } else {
-        throttleOut1.write(0.0);
-        throttleOut2.write(0.0);
+        throttleOut1.write_u16(0 * 6.5534);
+        throttleOut2.write_u16(0 * 6.5534);
     }
 }
 
@@ -73,16 +88,111 @@ void testADC(){
     analoginthrot2mv= analoginthrot2mv * 0.0763;        //5000mV full scale = 65535 * 0.0763
 }
 
+bool send_can1(){   
+    outMsg.id  = 0x1;
+    outMsg.len = 8;
+    for (int i = 0; i < outMsg.len; i++) {outMsg.Data[i] = 0x00;}
+    if(can1.write(CANMessage(outMsg.id, outMsg.Data, outMsg.len, CANData, CANStandard))){return true;}
+    else{return false;}
+}
+
+bool send_can2(){   
+    outMsg.id  = 0x2;
+    outMsg.len = 8;
+    for (int i = 0; i < outMsg.len; i++) {outMsg.Data[i] = 0x00;}
+    if(can2.write(CANMessage(outMsg.id, outMsg.Data, outMsg.len, CANData, CANStandard))){return true;}
+    else{return false;}
+}
+
+void can1Read(CANMessage inMsg){
+  printf("CAN 1 Receive\r\n");
+  switch (inMsg.id) {
+    case 0x3:
+      //variable = CAN_decode(&inMsg, 32, 16, LSB, SIGNED, 1, 0);
+    break;
+    default:
+      break;
+  }
+}
+
+void can2Read(CANMessage inMsg){
+  flashled();
+  printf("CAN 2 Receive\r\n");
+  switch (inMsg.id) {
+    case 0x3:
+      //variable = CAN_decode(&inMsg, 32, 16, LSB, SIGNED, 1, 0);
+    break;
+    default:
+      break;
+  }
+}
+
 void testECUfunctionality(){
-    flashled();
-    testDAC();
-    testUART();
-    testADC();
+    while (true){
+        //flashled();
+        testDAC();
+        testUART();
+        testADC();
+        send_can1();
+        send_can2();
+        ThisThread::sleep_for(200ms);
+    }
+}
+
+void testECUfunctionality2(){
+        //flashled();
+        testDAC();
+        testUART();
+        testADC();
+        send_can1();
+        send_can2();
+        return;
+}
+
+void checkcan(){
+    if(can1.read(inMsg)){can1Read(inMsg);}
+    if(can2.read(inMsg)){can2Read(inMsg);}
+}
+
+EventQueue queue;
+Event<void()> event1(&queue, testECUfunctionality2);
+Event<void()> event2(&queue, flashled);
+
+void post_events(void){
+    event1.post();      // Event1 with a value of 1
+    event2.post();      // Event1 with a value of 2
 }
 
 int main(){
-    while (true) {
-        ThisThread::sleep_for(repeatTime);
-        testECUfunctionality();
+
+    //EventQueue queue;
+
+    //queue.call_every(200ms, &testECUfunctionality);
+    //queue.call_every(1ms, &checkcan);
+    //queue.call_every(500ms, &testUART);
+    //queue.dispatch_forever();
+
+    //ms200.start(testECUfunctionality);
+
+    Thread event_thread;
+
+    event1.delay(10ms);       // Starting delay - 100 msec
+    event1.period(200ms);      // Delay between each event - 200msec
+
+    event2.delay(20ms);           // Starting delay - 400 msec
+    event2.period(100ms);   // Single non periodic event
+
+    event_thread.start(callback(post_events));
+    event_thread.join();
+    bool dispatchonce = false;
+
+    //Need to poll canbus as mbed ran rx interrupts are broken
+    while(true){
+        if(can1.read(inMsg)){can1Read(inMsg);}
+        if(can2.read(inMsg)){can2Read(inMsg);}
+        if(!dispatchonce){
+            queue.dispatch_forever();
+            dispatchonce = false;
+        }
     }
 }
--- vcupinmapp.h
+++ vcupinmapp.h
@@ -4,43 +4,43 @@
 #define hardwareVCU
 
 #ifdef hardwareVCU
-    #define can1rx PB_8
-    #define can1tx PB_9
-    #define can1nfault PC_4
-    #define can1nstb PC_5
-    #define can1en PB_0
-    #define can2rx PB_5
-    #define can2tx PB_13
+    #define M_CAN_RXD PB_8
+    #define M_CAN_TXD PB_9
+    #define M_CAN1_NFAULT PC_4
+    #define M_CAN1_NSTB PC_5
+    #define M_CAN1_EN PB_0
+    #define M_CAN2_RXD PB_5
+    #define M_CAN2_TXD PB_13
     #define statusledpin PC_13
-    #define dacout1 PA_4
-    #define dacout2 PA_5
-    #define analogin1 PC_0
-    #define analogin2 PC_1
-    #define analogin3 PC_2
-    #define analogin4 PC_3
-    #define throttlefeedback1 PB_1      //Used for BOOT1 - conflict!!!
-    #define throttlefeedback2 PB_10     //cant use PB_10 as ADC input!!!
-    #define hbridge1 PA_6   //to instantiate below down
-    #define hbridge2 PA_7
-    #define ctrlpilot PA_0
-    #define proxpilot PA_1
-    #define pwmout PB_12
-    #define output1 PC_9
-    #define output2 PC_7
-    #define output3 PC_6
-    #define output4 PB_14
-    #define output12stat PB_8
-    #define output34stat PB_15
-    #define ignwake PA_8
-    #define lintx PA_9
-    #define linrx PA_10
-    #define linenable PA_11
-    #define throt5vfdbk PA_12
-    #define degitalin1 PC_10
-    #define degitalin2 PC_11
-    #define degitalin3 PC_12
-    #define degitalin4 PD_2
-    #define chargenable PB_6
-    #define watchdog PB_7
+    #define M_DAC_OUT_1 PA_4
+    #define M_DAC_OUT_2 PA_5
+    #define M_ANA_IN_1 PC_0
+    #define M_ANA_IN_2 PC_1
+    #define M_ANA_IN_3 PC_2
+    #define M_ANA_IN_4 PC_3
+    #define M_DAC_FDBK_1 PB_1      //Used for BOOT1 - conflict!!!
+    #define M_DAC_FDBK_2 PB_10     //cant use PB_10 as ADC input!!!
+    #define H_BRIDGE_IN_1 PA_6   //to instantiate below down
+    #define H_BRIDGE_IN_2 PA_7
+    #define M_CTRL_PILOT PA_0
+    #define M_PROX_PILOT PA_1
+    #define M_PWM_OUT PB_12
+    #define OUT1_EN PC_9
+    #define OUT2_EN PC_7
+    #define OUT3_EN PC_6
+    #define OUT4_EN PB_14
+    #define OUT1_2_STAT PB_8
+    #define OUT3_4_STAT PB_15
+    #define M_IGN_WAKE PA_8
+    #define M_LIN_TXD PA_9
+    #define M_LIN_RXD PA_10
+    #define M_LIN_EN PA_11
+    #define M_THROT_5V PA_12
+    #define M_DIG_IN_1 PC_10
+    #define M_DIG_IN_2 PC_11
+    #define M_DIG_IN_3 PC_12
+    #define M_DIG_IN_4 PD_2
+    #define CHRG_EN PB_6
+    #define WDI PB_7
 #endif
 #endif
